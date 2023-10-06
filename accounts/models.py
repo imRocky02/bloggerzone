37acb8a67b1024645aace684fafd49cbdc4efc3d
Ritik Dhote <ritikdhote0b@gmail.com>
2023-10-06 11:53:03 +0530

    Admin panel name issue fixed

--- accounts/models.py
+++ accounts/models.py
@@ -1,56 +1,45 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager 
 from django.db import models
-
+from django.utils import timezone
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, full_name, dob, email, password=None):
+    def create_user(self,  email, password=None):
         """ 
         creates and save a user with the given email and password
         """
         if not email:
             raise ValueError("User must have an email address")
 
-        if not full_name:
-            raise ValueError("You have to fill name field")
-
-        if not dob:
-            raise ValueError("You have to fill dob field")
-
         user = self.model(
             email=self.normalize_email(email),
-            full_name = self.name(full_name),
-            dob = self.dates(dob),
+            
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
     
-    def create_staffuser(self, full_name, dob, email, password=None):
+    def create_staffuser(self,  email, password=None):
         """ 
         creates and save a staff user with the given email and password
         """
         user = self.create_user(
             email,
             password=password,
-            full_name =full_name,
-            dob =dob,
             
         )
         user.staff = True
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,  dob,  full_name, email, password=None):
+    def create_superuser(self,   email, password=None):
         """ 
         creates and save a superuser with the given email and password
         """
         user = self.create_user(
             email,
             password=password, 
-            full_name =full_name,
-            dob =dob,
         )
         user.staff = True
         user.admin = True
@@ -70,7 +59,7 @@ class CustomUser(AbstractBaseUser):
 
     full_name = models.CharField(max_length=250)
 
-    dob = models.DateField('Date OF Birth')
+    dob = models.DateField(verbose_name='Date OF Birth', default= timezone.now())
     
     is_active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False)
@@ -80,22 +69,22 @@ class CustomUser(AbstractBaseUser):
 
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = [full_name, dob]
 
 
     
-    def full_name(self):
-        return self.full_name
+    # def full_name(self):
+    #     return self.full_name
  
     
-    def dob(self):
-        return self.dob
+    # def dob(self):
+    #     return self.dob
 
-    def get_full_name(self):
-        return self.email
+        # def get_full_name(self):
+        #     return self.email
 
-    def get_short_name(self):
-        return self.email
+        # def get_short_name(self):
+        #     return self.email
 
     def __str__(self):
         return self.email
